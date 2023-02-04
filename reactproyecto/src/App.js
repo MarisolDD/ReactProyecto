3b66553c2521a4b354fdc3a3813977f6345be4b9
Duarte Edgardo Omar <duarteomar1975@gmail.com>
2023-02-04 16:57:12 -0300

    cambio texto en pantalla de prueba react

--- reactproyecto/src/App.js
+++ reactproyecto/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Primer Cambio de Prueba
         </a>
       </header>
     </div>
