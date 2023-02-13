8065b152d21daacef8a7452c99b5255cbf1bff4a
Duarte Edgardo Omar <duarteomar1975@gmail.com>
2023-02-13 19:04:56 -0300

    router-dom ok

--- reactproyecto/src/App.jsx
+++ reactproyecto/src/App.jsx
@@ -1,47 +1,63 @@
 import React from "react";
 import { Header } from "./componentes/Header";
 import "boxicons";
-import logo from "../src/images/Logo.jpg";
 import "./App.css";
 import { Footer } from "./componentes/Footer";
 import Cards from "./componentes/Cards";
 import "./App.css";
 import Carousel from "./componentes/carousel";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Contacto from "./componentes/Contacto.js";
 import { Body } from "./componentes/Body";
 import HeroImage from "./componentes/HeroImage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Contacto from "./pages/Contacto.jsx";
 
 function App() {
   return (
-    <div className="App h-100">
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/" element={[
+            <Header />,
+            <HeroImage />,
+            <Cards />,
+            <Carousel />,
+            <Body />,
+            <Footer />,
+          ]}
+        />
 
-      <div className="row">
-        <Header className="header" />
-      </div>
-
-      <div className="row">
-        <HeroImage/>
-      </div>
-
-      <div className="row mt-5">
-        <Cards />
-      </div>
-
-      <div className="row my-5">
-        <Carousel />
-      </div>
-
-      <div className="row h-50 m-5">
-        <Body />
-      </div>
-
-      <div className="row">
-        <Footer />
-      </div>
-
-    </div>
+        <Route path="/Contacto" element={<Contacto />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+/**   {/* <div className="App h-100"> */
+
+/* <div className="row">
+        <Header className="header" />
+      </div> */
+
+/* <div className="row">
+        <HeroImage/>
+      </div> */
+
+/* <div className="row mt-5">
+        <Cards />
+      </div> */
+
+/* <div className="row my-5">
+        <Carousel />
+      </div> */
+
+/* <div className="row h-50 m-5">
+        <Body />
+      </div> */
+
+/* <div className="row">
+        <Footer />
+      </div> */
+
+/* </div> */
--- reactproyecto/src/componentes/Header/index.jsx
+++ reactproyecto/src/componentes/Header/index.jsx
@@ -1,39 +1,43 @@
 import React from "react";
 import LOGO from "../../images/Logo.jpg";
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Outlet, Link } from "react-router-dom";
 
 export const Header = () => {
-    return (
-        <header>
-            <a href="http://localhost:3000/">
-                <div className="logo">
-                    <img src={LOGO} alt="logo" width="150"/>
-                </div>
-            </a>
-            <ul>
-                <li>
-                    <a href="http://localhost:3000/">DESTINOS</a>
-                </li>
-                <li>
-                    <a href="http://localhost:3000/">GRUPALES</a>
-                </li>
-                <li>
-                    <a href="http://localhost:3000/">AUTOS</a>
-                </li>
-                <li>
-                    <a href="http://localhost:3000/">SEGUROS</a>
-                </li>
-                <li>
-                <a href="http://localhost:3000/">CONTACTO</a>
-                    {/* <Link to={'../Contacto.js'}>Contacto</Link> */}
-                </li>
-            </ul>
-            <div className="cart">
-                <box-icon name="cart"></box-icon>
-                <span className="item_total">0</span>
-            </div>
-        </header>
-    )
-}
+  return (
+    <div className="container-fluid w-100">
+      <header>
+        <a href="http://localhost:3000/">
+          <div className="logo">
+            <img src={LOGO} alt="logo" width="150" />
+          </div>
+        </a>
+        <ul>
+          <li>
+            <a href="http://localhost:3000/">DESTINOS</a>
+          </li>
+          <li>
+            <a href="http://localhost:3000/">GRUPALES</a>
+          </li>
+          <li>
+            <a href="http://localhost:3000/">AUTOS</a>
+          </li>
+          <li>
+            <a href="http://localhost:3000/">SEGUROS</a>
+          </li>
+          <li>
+            {/* <a href="http://localhost:3000/">CONTACTO</a> */}
+            <Link to="contacto">Contacto</Link>
+          </li>
+        </ul>
+        <div className="cart">
+          <box-icon name="cart"></box-icon>
+          <span className="item_total">0</span>
+        </div>
 
+        <Outlet />
+      </header>
+    </div>
+  );
+};
