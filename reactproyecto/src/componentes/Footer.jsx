13f4b5e3f66a733086272d6ecbd69b56546ba029
Duarte Edgardo Omar <duarteomar1975@gmail.com>
2023-02-13 17:26:00 -0300

    layout + hero-image + estilos.css

---
+++ reactproyecto/src/componentes/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+// style={{ position: 'absolute', padding: '10% 10%', bottom: 0, width: '100%', textAlign: 'center' }}
+// style={{ position: 'absolute', padding: '10% 10%', bottom: 0, width: '100%', textAlign: 'center' }}  
+
+export const Footer = () => {
+    return (
+        <div className="footer-container" >
+            <footer className="py-3 my-4" >
+                <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                <li className="nav-item"><a href="http://localhost:3000/" className="nav-link px-2 text-muted">DESTINO</a></li>
+                <li className="nav-item"><a href="http://localhost:3000/" className="nav-link px-2 text-muted">GRUPALES</a></li>
+                <li className="nav-item"><a href="http://localhost:3000/" className="nav-link px-2 text-muted">AUTOS</a></li>
+                <li className="nav-item"><a href="http://localhost:3000/" className="nav-link px-2 text-muted">SEGUROS</a></li>
+                <li className="nav-item"><a href="http://localhost:3000/" className="nav-link px-2 text-muted">NOSOTROS</a></li>
+                </ul>
+                <p className="text-center text-muted">&copy; 2023 Company, Inc</p>
+            </footer>
+        </div>
+    )
+}
+
--- reactproyecto/src/componentes/Header/index.jsx
+++ reactproyecto/src/componentes/Header/index.jsx
@@ -1,31 +1,32 @@
 import React from "react";
 import LOGO from "../../images/Logo.jpg";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export const Header = () => {
     return (
         <header>
-            <a href="#">
+            <a href="http://localhost:3000/">
                 <div className="logo">
                     <img src={LOGO} alt="logo" width="150"/>
                 </div>
             </a>
             <ul>
                 <li>
-                    <a href="#">DESTINOS</a>
+                    <a href="http://localhost:3000/">DESTINOS</a>
                 </li>
                 <li>
-                    <a href="#">GRUPALES</a>
+                    <a href="http://localhost:3000/">GRUPALES</a>
                 </li>
                 <li>
-                    <a href="#">AUTOS</a>
+                    <a href="http://localhost:3000/">AUTOS</a>
                 </li>
                 <li>
-                    <a href="#">SEGUROS</a>
+                    <a href="http://localhost:3000/">SEGUROS</a>
                 </li>
                 <li>
-                    <Link to={'../Contacto.js'}>Contacto</Link>
+                <a href="http://localhost:3000/">CONTACTO</a>
+                    {/* <Link to={'../Contacto.js'}>Contacto</Link> */}
                 </li>
             </ul>
             <div className="cart">
