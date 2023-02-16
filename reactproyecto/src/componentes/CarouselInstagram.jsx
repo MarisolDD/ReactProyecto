6947c8032e198b19aa69a9208c67cbeefc33ea50
Duarte Edgardo Omar <duarteomar1975@gmail.com>
2023-02-16 19:25:39 -0300

    cargo token meta for developers y todo ok!

--- reactproyecto/src/componentes/CarouselInstagram.jsx
+++ reactproyecto/src/componentes/CarouselInstagram.jsx
@@ -7,7 +7,7 @@ const CarouselInstagram = () => {
 
   /**useEffect para llamar api instagram */
   useEffect(() => {
-    let accessToken ="COLOCAR EL TOKEN AQUI";
+    let accessToken ="IGQVJXOWJKalRtekhOdks1YXNCUk1DY00tS0xYRnZAzaVlsY1VqRGNacmVHTjF6TjBpNENtcVhIbGo5T2gzdzIxU05nU0YwSndKbEpfRlhmUXVHeVpGd3haTnNkTHVJOEJQRHNGbkc4RUMtcHdIN3BTMQZDZD";
 
     let url =
       "https://graph.instagram.com/me/media?fields=id,caption,media_url&access_token=";
