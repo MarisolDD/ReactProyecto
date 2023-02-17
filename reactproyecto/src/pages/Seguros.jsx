a9859504a4fd69883ed51b8a0771f60bd871491a
Estani94 <estanislaotalentilopez@gmail.com>
2023-02-17 11:39:38 -0300

    Prueba con Seguros

--- reactproyecto/src/pages/Seguros.jsx
+++ reactproyecto/src/pages/Seguros.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Navbar } from '../componentes/Navbar';
 import { Link } from "react-router-dom";
+import { Footer } from '../componentes/Footer';
+import Cards from '../componentes/Cards';
 
 export const Seguros = () => {
   return (
-    <Navbar/>
+    <>
+      <Navbar/>
+      <Cards/>
+      <Footer/>
+    </>
   )
 }
