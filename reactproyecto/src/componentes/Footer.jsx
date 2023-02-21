0189e81cc58f6468f9544a683e208b1d5bfc8947
Estani94 <estanislaotalentilopez@gmail.com>
2023-02-21 15:52:58 -0300

    Seguros con footer

--- reactproyecto/src/componentes/Footer.jsx
+++ reactproyecto/src/componentes/Footer.jsx
@@ -9,26 +9,17 @@ const Footer = () => {
       <footer className="py-3 my-4">
         <ul className="nav justify-content-center border-bottom pb-3 mb-3">
           <li className="nav-item">
-            <a
-              href="http://localhost:3000/"
-              className="nav-link px-2 text-muted"
-            >
+            <a href="http://localhost:3000/" className="nav-link px-2 text-muted">
               DESTINO
             </a>
           </li>
           <li className="nav-item">
-            <a
-              href="http://localhost:3000/"
-              className="nav-link px-2 text-muted"
-            >
+            <a href="http://localhost:3000/" className="nav-link px-2 text-muted">
               GRUPALES
             </a>
           </li>
           <li className="nav-item">
-            <a
-              href="http://localhost:3000/"
-              className="nav-link px-2 text-muted"
-            >
+            <a href="http://localhost:3000/" className="nav-link px-2 text-muted">
               AUTOS
             </a>
           </li>
