d5510cbc872ec1e605291acc8c0399fa5c53c0ac
Duarte Edgardo Omar <duarteomar1975@gmail.com>
2023-02-22 17:43:29 -0300

    variable de entorno ok

--- reactproyecto/src/componentes/CarouselInstagram.jsx
+++ reactproyecto/src/componentes/CarouselInstagram.jsx
@@ -7,8 +7,8 @@ const CarouselInstagram = () => {
 
   /**useEffect para llamar api instagram */
   useEffect(() => {
-    let accessToken =
-      "IGQVJYb3duWkswMmJKMFc2ZA0lGOUR1b2tjYWlZATjNaaWQyLVFRZAHdoMjlVOHotWFg3dFFsamZASWmFXRTRnV2dpcERzUEZAWNUYzZAUxUR05mUnhxUUxiVzVkUmotRmVBVTQ2TjlDOUlPcnlya0xpbnZAXWQZDZD";
+    let accessToken = process.env.REACT_APP_TOKEN_INSTAGRAM
+      // "IGQVJYb3duWkswMmJKMFc2ZA0lGOUR1b2tjYWlZATjNaaWQyLVFRZAHdoMjlVOHotWFg3dFFsamZASWmFXRTRnV2dpcERzUEZAWNUYzZAUxUR05mUnhxUUxiVzVkUmotRmVBVTQ2TjlDOUlPcnlya0xpbnZAXWQZDZD";
 
     let url =
       "https://graph.instagram.com/me/media?fields=id,caption,media_url&access_token=";
