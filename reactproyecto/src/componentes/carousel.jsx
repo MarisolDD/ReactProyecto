2eda4772e83809881277ee42cd481c6e1163d244
Duarte Edgardo Omar <duarteomar1975@gmail.com>
2023-02-25 16:41:37 -0300

    carousel ok

--- reactproyecto/src/componentes/carousel.jsx
+++ reactproyecto/src/componentes/carousel.jsx
@@ -27,15 +27,15 @@ const Carousel = () => {
 
   return (
 
-    <div className="container-fluid w-100 miCarousel mt-5">
+    <div className="container-fluid w-100 miCarousel mt-2">
       <div className="row">
         <h1 className="h1 text-center">Los destinos mas elegidos por nuestros clientes</h1>
         </div>
       <motion.div drag="x" dragConstraints={{ right: 900, left: -10200 }} className="d-flex justify-content-between align-items-center flex-nowrap">
       {arreglo.map((e, index) => {
         return (
-            <motion.div className="item bg-dark mx-2 text-warning rounded-5 miTarjeta2" key={index}>
-              <div className="col-3 bg-warning miTarjeta p-1 m-5">
+            <motion.div className="item mx-1 rounded-5" key={index}>
+              <div className="col-3 miTarjeta p-1 m-5">
                 <img
                   src={`../assets/${e.src}`}
                   alt="imagen"
