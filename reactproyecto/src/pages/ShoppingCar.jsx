964dd7e270e465112d3c6803fa129f3c0d250dad
MarisolDD <87marisoldiaz@gmail.com>
2023-02-26 17:52:29 -0300

    cambiosCSS

--- reactproyecto/src/pages/ShoppingCar.jsx
+++ reactproyecto/src/pages/ShoppingCar.jsx
@@ -1,5 +1,6 @@
 import React, { useReducer } from "react";
 import { TYPES } from "../componentes/actioncar";
+import "./ShoppingCar.css";
 import { cartInitialState, cartReducer } from "../componentes/cartReducer";
 import Producto from "../componentes/Producto";
 import ItemCarrito from "../componentes/ItemCarrito";
@@ -24,15 +25,15 @@ const ShoppingCar = () => {
   };
 
   return (
-    <div className="container-fluid">
+    <div className="container-fluid" id = "contenedor1">
       <div className="row">
         <div className="row text-center mt-5">
-          <h2 className="h2 mt-5">Carrito de compras</h2>
-          <h3 className="h3">Productos</h3>
+          <h2 className="h2 mt-5" id = "carrito">Carrito de compras</h2>
+          <h3 className="h3" >Productos</h3>
         </div>
         <div className="row">
           {products.map((producto) => (
-            <div className="col-12 col-md-6 col-xl-3">
+            <div className="col-12 col-md-6 col-xl-3" id ="productos">
               <Producto
                 key={producto.id}
                 data={producto}
