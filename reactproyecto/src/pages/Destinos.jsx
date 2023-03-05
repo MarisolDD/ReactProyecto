288046591e26eb8a853783674357f11c98d182f0
Duarte Edgardo Omar <duarteomar1975@gmail.com>
2023-03-05 19:24:44 -0300

    ajustes paginas en construccion + video

--- reactproyecto/src/pages/Destinos.jsx
+++ reactproyecto/src/pages/Destinos.jsx
@@ -6,11 +6,18 @@ import video from "./assets/videos/VOLANDO sobre ARGENTINA en un minuto _ 4K _.m
 
 function Destinos() {
   return (
- 
-    <div className=''>
-    <ReactPlayer url={video} controls loop muted volume={1} width="100%" height="90%"/>
+    <div className='container-fluid d-flex flex-column align-items-around justify-content-around  bg-dark text-white text-center' style={{height:'100vh', width:'100vw'}}>
+    <div className='row' style={{height:'13%', width:'100%'}}> 
+    </div>
+    <div className='row text-warning'>
+      <h5>Conozca más al respecto de nuestras promociones por multidestino y acumulación de millas aéreas </h5>
+    </div>
+
+    <div className='row mx-auto d-block h-75'>
+      <ReactPlayer url={video} controls loop   volume={0.1} width='100%' height='100%'/>
+    </div>
        
-    <h3>Conozca más al respecto de nuestras promociones por multidestino y acumulación de millas aéreas </h3>
+
   </div>
 
   )
