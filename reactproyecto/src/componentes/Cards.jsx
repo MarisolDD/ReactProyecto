5dc8abb8ed4b7c711152e5470fa1730bb033485b
Duarte Edgardo Omar <duarteomar1975@gmail.com>
2023-03-08 16:28:23 -0300

    cards con axios + json-server ok

--- reactproyecto/src/componentes/Cards.jsx
+++ reactproyecto/src/componentes/Cards.jsx
@@ -1,53 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
-
-import image1 from "../images/iguazu.jpeg";
-import image2 from "../images/bariloche.jpeg";
-import image3 from "../images/jujuy.jpeg";
-import image4 from "../images/calafate.jpeg";
-
-const cards = [
-  {
-    id: 1,
-    title: "Iguazu",
-    image: image1,
-    url: "https://misiones.tur.ar/iguazu/",
-    text: "",
-  },
-  {
-    id: 2,
-    title: "Bariloche",
-    image: image2,
-    url: "https://barilocheturismo.gob.ar/es/home",
-    text: "",
-  },
-  {
-    id: 3,
-    title: "Jujuy",
-    image: image3,
-    url: "http://www.turismo.jujuy.gob.ar/",
-    text: "",
-  },
-  {
-    id: 4,
-    title: "Calafate",
-    image: image4,
-    url: "https://www.elcalafate.tur.ar/",
-    text: "",
-  },
-];
+import axios from 'axios'
 
 const Cards = () => {
+
+
+  const[arrayInfoCards, setArrayInfoCards]=useState([])
+
+  useEffect(()=>{
+    let path="http://localhost:5555/info-cards"
+
+    const getInfoCards = async () =>{
+
+      try {
+        
+        const res = await axios.get(path)
+
+        setArrayInfoCards(res.data)
+
+        console.log(res.data)
+
+      } catch (error) {
+        alert(error.message)
+      }
+
+    }
+
+    getInfoCards()
+  },[])
+
 
   return (
     <div className="container d-flex justify-content-center align-items-center mt-5">
       <div className="row mt-4">
-        {cards.map((card) => (
+        {arrayInfoCards.map((card) => (
           <div className="col-md-3" key={card.id}>
             <Card
               id={card.id}
               title={card.title}
-              imageSource={card.image}
+              imageSource={`../assets/${card.image}`}
               url={card.url}
               text={card.text}
             ></Card>
